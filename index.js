a5129246c178f5c1c44281122a39f3c43f55ac95
Mr. Pavel <pavel@elblaus.ru>
2022-12-18 00:52:51 +0300

    update core app v15.14.0

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 "use strict";
-// импорт базовых модулей
+// загрузка базовых модулей
+const { print } = require("./lib/tools.js");
 const { init } = require("./lib/app.js");
+
+// фикс для собранного приложения, иначе не может найти пути из app и source модулей
+global.require_path = path => require(path);
+// глобальные пути к библиотекам собранного приложения, для передачи в worker
+global.require_paths = module.paths;
+
+// пишем клиенту
+print("Start development mode version \x1b[33m"+process.env.PDB_VERSION+"\x1b[0m ...");
 
 // экспорт
 module.exports = init;
