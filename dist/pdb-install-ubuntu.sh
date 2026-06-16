651a51bc9bab1f6bdd4db92a7de699b74f4262cf
Mr. Pavel <pavel@elblaus.ru>
2026-06-16 12:26:25 +0300

    disable IPv6 by default in Ubuntu installer

--- dist/pdb-install-ubuntu.sh
+++ dist/pdb-install-ubuntu.sh
@@ -60,6 +60,17 @@ if [ -z "$(swapon -s)" ] && [ $memory -lt 3000000 ]; then
 fi
 
 #----------------------------------------------------------#
+#                      Disabling IPv6                      #
+#----------------------------------------------------------#
+
+sudo tee /etc/sysctl.d/99-disable-ipv6.conf > /dev/null <<EOF
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+EOF
+sudo sysctl --system
+
+#----------------------------------------------------------#
 #                   Install repository                     #
 #----------------------------------------------------------#
 
